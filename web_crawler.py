fd4a9b3b0381e207cab10caf9e913b00477b0deb
Kai <qaz093351365@gmail.com>
2016-05-29 20:34:49 +0800

    New:save file

--- web_crawler.py
+++ web_crawler.py
@@ -3,6 +3,7 @@ import queue
 import requests
 import sys
 import time
+import os
 links=[]
 emails=[]
 box=queue.Queue(maxsize=10)
@@ -18,35 +19,57 @@ class Newlink(threading.Thread):
     def run(self):
         self.cond.acquire()
         print("New Link=>{}".format(self.url))
-        link_re=re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-f]))+') 
-        #link_re=re.compile('href="(.*?)"')
+        link_re2=re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-f]))+') 
+        link_re=re.compile('href="(.*?)"')
         email_re=re.compile('([\w\.,]+@[\w\.,]+\.\w+)')
-        req=requests.get(self.url)
-        if req.status_code == 200:
-            getlinks=link_re.findall(req.text)
-            #links2=re.search(link_re,req.text)
-            getemails=email_re.findall(req.text)
+        try:
+            req=requests.get(self.url)
+            if req.status_code == 200:
+                getlinks=link_re.findall(req.text)
+                getemails=email_re.findall(req.text)
 
-            for i in getemails:
-                if i in emails:
-                    continue
-                else:
-                    emails.append(i)
-            #print("===========Links===============")
-            for i in getlinks:
-                if i in links:
-                    continue
-                else:
-                    links.append(i)
-                    #print("start to "+i)
-                    Newlink(i,cond).start()
-            self.cond.release()
-            print("===========Email===============") 
-            for i in emails:
-                print(i)
-        else:
-            self.cond.release()
-            print("done")
+                for i in getemails:
+                    if i in emails:
+                        continue
+                    else:
+                        emails.append(i)
+                #print("===========Links===============")
+                data=open('link_log.txt','a')
+                for i in getlinks:
+                    chk=re.match("http",i)
+                    if chk:
+                        if i in links:
+                            continue
+                        else:
+                            links.append(i)
+                            data.write(i)
+                            data.write('\n')
+                            #print("start to "+i)
+                            Newlink(i,cond).start()
+                    else:
+                        newi=self.url+i
+                        if newi in links:
+                            continue
+                        else:
+                            links.append(newi)
+                            data.write(newi)
+                            data.write('\n')
+                            #print("start to "+i)
+                            Newlink(newi,cond).start()
+                data.close()
+                self.cond.release()
+                print("===========Email===============") 
+                data=open('e-mail_log.txt','w')
+                for i in emails:
+                    data.write(i)
+                    data.write("\n")
+                    print(i)
+                data.close()
+            else:
+                self.cond.release()
+                print("done")
+        except requests.exceptions.ConnectionError as e:
+            pass
         
 if __name__ == "__main__":
 
